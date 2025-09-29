83f51f8bf810991c9be4975b2e147c94942ecabf
Diego <diego.cainzos@udc.com>
2025-09-29 17:06:08 +0300

    chore: initial production set up

---
+++ script.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+### EMAIL
+def send_email(subject, body):
+    # Email configuration
+    sender_email = "digarca02@gmail.com"
+    sender_password = "syrz icyp ztpo putv" 
+    receiver_email = "digarca02@gmail.com"
+    
+    # Create message
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    
+    message.attach(MIMEText(body, "plain"))
+    
+    # Send email
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.quit()
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+url = "https://immi.homeaffairs.gov.au/what-we-do/whm-program/status-of-country-caps"
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser')
+spain_td = soup.find("td", string="Spain")
+status = spain_td.next_sibling.text
+
+if "paused" not in status:
+    print("ya no esta pausado")
+else:
+    print("sigue pausado")
+    send_email("sigue pausado nano", "Spain WHM Not Available!")
